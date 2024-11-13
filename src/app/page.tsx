b96a7e8f3b1f08954f753a71a95b30f6962955b1
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-13 09:31:21 +0100

    added function to return an empty array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+function createDogService() {
+  return {
+    async getAll() {
+      return [1];
+    },
+  };
+}
+
+const dogService = createDogService();
 export default async function Home() {
-  const dogs = await [];
-  return (
+  const dogs = await dogService.getAll();
+  return (  
     <>
       <h1>Hello World</h1>
       {!dogs.length && <p>Get more dogs man, sad face</p>}
